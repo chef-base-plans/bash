75698a23c0cf39d7cebc448f2038a8bce1856731
Sangameshwar Mandakanalli <smandaka@progress.com>
2021-10-26 12:16:54 +0530

    Version bum from 5.1.4 to 5.1.8
    Signed-off-by: Sangameshwar Mandakanalli <smandaka@progress.com>

--- bash-patches.sh
+++ bash-patches.sh
@@ -7,6 +7,10 @@ _patch_files=(
   "${_patch_url_base}-002"
   "${_patch_url_base}-003"
   "${_patch_url_base}-004"
+  "${_patch_url_base}-005"
+  "${_patch_url_base}-006"
+  "${_patch_url_base}-007"
+  "${_patch_url_base}-008"
 )
 
 # All official patch file shasums
@@ -15,4 +19,8 @@ _patch_shasums=(
   15ea6121a801e48e658ceee712ea9b88d4ded022046a6147550790caf04f5dbe
   22f2cc262f056b22966281babf4b0a2f84cb7dd2223422e5dcd013c3dcbab6b1
   9aaeb65664ef0d28c0067e47ba5652b518298b3b92d33327d84b98b28d873c86
+  cccbb5e9e6763915d232d29c713007a62b06e65126e3dd2d1128a0dc5ef46da5
+  75e17d937de862615c6375def40a7574462210dce88cf741f660e2cc29473d14
+  acfcb8c7e9f73457c0fb12324afb613785e0c9cef3315c9bbab4be702f40393a
+  f22cf3c51a28f084a25aef28950e8777489072628f972b12643b4534a17ed2d1
 )
